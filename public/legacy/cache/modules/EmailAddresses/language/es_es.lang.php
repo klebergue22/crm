e62f0c81c09bda48990091daf42b12759fd401d6
Kleber Guerra <kleber.guerra@geograficomilitar.gob.ec>
2026-06-29 10:12:30 -0500

    Traducir EmailAddresses al español

---
+++ public/legacy/cache/modules/EmailAddresses/language/es_es.lang.php
@@ -0,0 +1,13 @@
+<?php
+// created: 2026-06-29 00:00:00
+$mod_strings = array (
+  'LBL_EMAIL_ADDRESS_ID' => 'ID',
+  'LBL_EMAIL_ADDRESS' => 'Dirección de correo',
+  'LBL_EMAIL_ADDRESS_CAPS' => 'Dirección de correo en mayúsculas',
+  'LBL_INVALID_EMAIL' => 'Correo no válido',
+  'LBL_OPT_OUT' => 'Excluido',
+  'LBL_CONFIRM_OPT_IN' => 'Confirmar inclusión',
+  'LBL_DATE_CREATE' => 'Fecha de creación',
+  'LBL_DATE_MODIFIED' => 'Fecha de modificación',
+  'LBL_DELETED' => 'Eliminar',
+);
