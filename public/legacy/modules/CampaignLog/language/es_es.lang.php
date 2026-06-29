f06177a4feec27588262095cacc2efdeb15d5dfc
Kleber Guerra <kleber.guerra@geograficomilitar.gob.ec>
2026-06-29 09:40:25 -0500

    Traducir CampaignLog al español

---
+++ public/legacy/modules/CampaignLog/language/es_es.lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$mod_strings = [
+    'LBL_MODULE_NAME' => 'Registro de campañas',
+    'LBL_CAMPAIGN_NAME' => 'Nombre:',
+    'LBL_CAMPAIGN' => 'Campaña:',
+    'LBL_LIST_CAMPAIGN_NAME' => 'Campaña',
+    'LBL_CAMPAIGN_START_DATE' => 'Fecha de inicio:',
+    'LBL_CAMPAIGN_END_DATE' => 'Fecha de fin:',
+    'LBL_CAMPAIGN_STATUS' => 'Estado:',
+    'LBL_CAMPAIGN_BUDGET' => 'Presupuesto:',
+    'LBL_CAMPAIGN_EXPECTED_COST' => 'Costo esperado:',
+    'LBL_CAMPAIGN_ACTUAL_COST' => 'Costo real:',
+    'LBL_CAMPAIGN_EXPECTED_REVENUE' => 'Ingresos esperados:',
+    'LBL_CAMPAIGN_TYPE' => 'Tipo:',
+    'LBL_CAMPAIGN_OBJECTIVE' => 'Objetivo:',
+    'LBL_CAMPAIGN_CONTENT' => 'Descripción:',
+    'LBL_LIST_FORM_TITLE' => 'Campañas objetivo',
+    'LBL_MORE_INFO' => 'Más información',
+    'LBL_CAMPAIGNS' => 'Campañas',
+];
