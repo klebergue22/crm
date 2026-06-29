85c204bf6253416b6eb059c23a73fa87f1574533
Kleber Guerra <kleber.guerra@geograficomilitar.gob.ec>
2026-06-29 09:35:23 -0500

    Traducir CRM al español

---
+++ public/legacy/cache/modules/Documents/language/es_es.lang.php
@@ -0,0 +1,24 @@
+<?php
+// created: 2026-06-23 21:01:06
+$mod_strings = array (
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Fecha de creación',
+  'LBL_DATE_MODIFIED' => 'Fecha de modificación',
+  'LBL_MODIFIED' => 'Modificado por',
+  'LBL_MODIFIED_NAME' => 'Modificado por nombre',
+  'LBL_CREATED' => 'Creado por',
+  'LBL_DESCRIPTION' => 'Descripción',
+  'LBL_DELETED' => 'Eliminado',
+  'LBL_NAME' => 'Nombre',
+  'LBL_CREATED_USER' => 'Usuario que creó',
+  'LBL_MODIFIED_USER' => 'Usuario que modificó',
+  'LBL_LIST_NAME' => 'Nombre',
+  'LBL_EDIT_BUTTON' => 'Editar',
+  'LBL_REMOVE' => 'Eliminar',
+  'LBL_ASCENDING' => 'Ascendente',
+  'LBL_DESCENDING' => 'Descendente',
+  'LBL_OPT_IN' => 'Aceptar',
+  'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Pendiente de confirmacion, correo no enviado',
+  'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Pendiente de confirmacion, correo enviado',
+  'LBL_OPT_IN_CONFIRMED' => 'Aceptado',
+);
